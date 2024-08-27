a8f4a13b7ba4abf59d94f5edbf105bcc7ae6a1d9
skydev111 <skydev359@outlook.com>
2024-08-26 22:13:10 -0700

    feat: model build

--- linear_regression.py
+++ linear_regression.py
@@ -24,3 +24,32 @@ train_X = data[:, 2][:train_test_split] #Percent unemployed
 train_Y = data[:, 3][:train_test_split] #Murders per 1 million population per year
 n_samples = train_X.shape[0]
 
+# Create placeholder for providing inputs
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+# create weights and bias and initialize with random number
+W = tf.Variable(rng.randn(), name="weight")
+b = tf.Variable(rng.randn(), name="bias")
+
+with tf.name_scope('WX_b') as scope:
+    # Construct a linear model using Y=WX+b
+    pred = tf.add(tf.multiply(X, W), b)
+
+w_h = tf.summary.histogram('weights', W)
+b_h = tf.summary.histogram('biases', b)
+
+with tf.name_scope('cost_function') as scope:
+    # Calculate Mean squared error
+    cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
+    tf.summary.scalar('cost_function', cost)
+
+with tf.name_scope('train') as scope:
+    # Gradient descent to minimize mean sequare error
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+marge_summary_op = tf.summary.merge_all()
+
