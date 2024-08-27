850fc6ce362084d8f3bfbda93753325688c61490
skydev111 <skydev359@outlook.com>
2024-08-26 22:15:59 -0700

    feat: build logistic model

--- logistic_regression.py
+++ logistic_regression.py
@@ -17,3 +17,22 @@ training_epochs = 25
 batch_size = 100
 display_step = 1
 
+# tf Graph Input
+x = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
+y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
+
+# Set model weights
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+# Construct model
+pred = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax for output probablity
+
+# Minimize error using cross entropy
+cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
+# Gradient Descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
